a27a6ed7d98d6037edea358953856a645cbbced7
Marcel <marcel.meurer@sepago.de>
2018-06-13 21:51:50 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -13,11 +13,44 @@ C code : test.cpp
 
 int main(int argc, char *argv[])
 {
-	int RXPIN = 1;
+	int RXPIN = 2;
 	int TXPIN = 0;
         int loggingok;   // Global var indicating logging on or off
         FILE *fp;        // Global var file handle
         
+    /* default values */
+    char  MQTT_HOSTNAME[] = "localhost\0";
+    char  MQTT_USERTNAME[] = "admin\0";
+    char  MQTT_PASSWORD[] = "password\0";
+    char  MQTT_TOPIC[]    = "433MHz\0";
+    char* host = MQTT_HOSTNAME; 
+    int port   = 1883;
+    char* usr  = MQTT_USERTNAME; 
+    char* pswd = MQTT_PASSWORD; 
+    char* topic= MQTT_TOPIC;
+	
+    // Initialize the Mosquitto library
+    mosquitto_lib_init();
+
+    // Create a new Mosquitto runtime instance with a random client ID,
+    //  and no application-specific callback data.  
+    mosq = mosquitto_new (NULL, true, NULL);
+    if (!mosq)
+    {
+      fprintf (stderr, "Can't initialize Mosquitto library\n");
+      exit (-1);
+    }
+    
+    mosquitto_username_pw_set (mosq, usr, pswd);
+
+    // Establish a connection to the MQTT server. Do not use a keep-alive ping
+    int ret = mosquitto_connect (mosq, host, port, 0);
+    if (ret)
+    {
+      fprintf (stderr, "Can't connect to Mosquitto server\n");
+      exit (-1);
+    }
+	
         if(argc==2) {
           fp = fopen(argv[1], "a"); // Log file opened in append mode to avoid destroying data
           loggingok=1;
