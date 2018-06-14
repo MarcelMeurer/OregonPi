6e29ef2bfd05475ff92af9857e2facfac34ea578
Marcel <marcel.meurer@sepago.de>
2018-06-14 22:31:18 +0200

    Update RCSwitch.cpp

--- RCSwitch.cpp
+++ RCSwitch.cpp
@@ -151,7 +151,7 @@ void RCSwitch::handleInterrupt() {
   if ( !OokAvailableCode ) {		// avoid reentrance -- wait until data is read
 	  if (orscV2.nextPulse(p)) 	{ RCSwitch::OokAvailableCode = true; orscV2.sprint("OSV2 ",RCSwitch::OokReceivedCode); orscV2.resetDecoder(); }
 	  if (orscV3.nextPulse(p)) 	{ RCSwitch::OokAvailableCode = true; orscV3.sprint("OSV3 ",RCSwitch::OokReceivedCode); orscV3.resetDecoder(); }
-	  if (rcswp1.nextPulse(p)) 	{ RCSwitch::OokAvailableCode = true; rcswp1.sprint("ALRM ",RCSwitch::OokReceivedCode); rcswp1.resetDecoder(); }
+	//  if (rcswp1.nextPulse(p)) 	{ RCSwitch::OokAvailableCode = true; rcswp1.sprint("ALRM ",RCSwitch::OokReceivedCode); rcswp1.resetDecoder(); }
 
 	//  if (cres.nextPulse(p)) 	{ cres.print("CRES"); cres.resetDecoder(); }
 	//  if (kaku.nextPulse(p)) 	{ kaku.print("KAKU"); kaku.resetDecoder(); }
