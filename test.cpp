033f8a9dc23bd78f728a3c25699566c510e8d755
Marcel <marcel.meurer@sepago.de>
2018-06-14 22:07:50 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,46 +3,45 @@ C code : test.cpp
 * ===================================================
 */
 
-
-#include <stdlib.h> 
-#include <string.h> 
-#include <stdio.h> 
-#include <time.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
 #include "RCSwitch.h"
 #include "RcOok.h"
 #include "Sensor.h"
 
 /* inlcude MQTT */
-
 #include <mosquitto.h>
 
-  int main(int argc, char * argv[]) {
+int main(int argc, char* argv[])
+{
     int RXPIN = 2;
     int TXPIN = 0;
     int loggingok; // Global var indicating logging on or off
-    FILE * fp; // Global var file handle
+    FILE* fp; // Global var file handle
 
     /* default values */
     char MQTT_HOSTNAME[] = "localhost\0";
     char MQTT_USERTNAME[] = "admin\0";
     char MQTT_PASSWORD[] = "password\0";
     char MQTT_TOPIC[] = "433MHz\0";
-    char * host = MQTT_HOSTNAME;
+    char* host = MQTT_HOSTNAME;
     int port = 1883;
-    char * usr = MQTT_USERTNAME;
-    char * pswd = MQTT_PASSWORD;
-    char * topic = MQTT_TOPIC;
-    struct mosquitto * mosq = NULL;
+    char* usr = MQTT_USERTNAME;
+    char* pswd = MQTT_PASSWORD;
+    char* topic = MQTT_TOPIC;
+    struct mosquitto* mosq = NULL;
 
     // Initialize the Mosquitto library
     mosquitto_lib_init();
 
     // Create a new Mosquitto runtime instance with a random client ID,
-    //  and no application-specific callback data.  
+    //  and no application-specific callback data.
     mosq = mosquitto_new(NULL, true, NULL);
     if (!mosq) {
-      fprintf(stderr, "Can't initialize Mosquitto library\n");
-      exit(-1);
+        fprintf(stderr, "Can't initialize Mosquitto library\n");
+        exit(-1);
     }
 
     mosquitto_username_pw_set(mosq, usr, pswd);
@@ -50,57 +49,61 @@ C code : test.cpp
     // Establish a connection to the MQTT server. Do not use a keep-alive ping
     int ret = mosquitto_connect(mosq, host, port, 0);
     if (ret) {
-      fprintf(stderr, "Can't connect to Mosquitto server\n");
-      exit(-1);
+        fprintf(stderr, "Can't connect to Mosquitto server\n");
+        exit(-1);
     }
 
+    char valueStr[48];
+
     if (argc == 2) {
-      fp = fopen(argv[1], "a"); // Log file opened in append mode to avoid destroying data
-      loggingok = 1;
-      if (fp == NULL) {
-        perror("Failed to open log file!"); // Exit if file open fails
-        exit(EXIT_FAILURE);
-      }
-    } else {
-      loggingok = 0;
+        fp = fopen(argv[1], "a"); // Log file opened in append mode to avoid destroying data
+        loggingok = 1;
+        if (fp == NULL) {
+            perror("Failed to open log file!"); // Exit if file open fails
+            exit(EXIT_FAILURE);
+        }
+    }
+    else {
+        loggingok = 0;
     }
 
     if (wiringPiSetup() == -1)
-      return 0;
+        return 0;
 
-    RCSwitch * rc = new RCSwitch(RXPIN, TXPIN);
+    RCSwitch* rc = new RCSwitch(RXPIN, TXPIN);
 
     while (1) {
-      if (rc - > OokAvailable()) {
-        char message[100];
+        if (rc->OokAvailable()) {
+            char message[100];
 
-        rc - > getOokCode(message);
-        printf("%s\n", message);
+            rc->getOokCode(message);
+            printf("%s\n", message);
 
-        Sensor * s = Sensor::getRightSensor(message);
-        if (s != NULL) {
-          printf("Temp : %f\n", s - > getTemperature());
-          printf("Humidity : %f\n", s - > getHumidity());
-          printf("Channel : %d\n", s - > getChannel());
-          if ((loggingok) && (s - > getChannel() > 0)) {
-            fprintf(fp, "OREGON#{\"channel\":%d,\"temp\":%f,\"humidity\":%f}\n", s - > getChannel(), s - > getTemperature(), s - > getHumidity());
-            fflush(fp);
-            fflush(stdout);
-          }
+            Sensor* s = Sensor::getRightSensor(message);
+            if (s != NULL) {
+                printf("Temp : %f\n", s->getTemperature());
+                printf("Humidity : %f\n", s->getHumidity());
+                printf("Channel : %d\n", s->getChannel());
+                if ((loggingok) && (s->getChannel() > 0)) {
+                    fprintf(fp, "OREGON#{\"channel\":%d,\"temp\":%f,\"humidity\":%f}\n", s->getChannel(), s->getTemperature(), s->getHumidity());
+                    fflush(fp);
+                    fflush(stdout);
+                }
+            }
+
+            //char  valueStr[35];
+
+            sprintf(valueStr, "OREGON#{\"channel\":%d,\"temp\":%f,\"humidity\":%f}", s->getChannel(), s->getTemperature(), s->getHumidity());
+            ret = mosquitto_publish(mosq, NULL, &MQTT_TOPIC[0u], strlen(valueStr), valueStr, 0, false);
+            if (ret) {
+                fprintf(stderr, "Can't publish to Mosquitto server\n");
+                exit(-1);
+            }
+            else {
+                fprintf(stdout, "%s", valueStr);
+            }
+            delete s;
         }
-
-        char  valueStr[120];
-
-        sprintf(valueStr, "OREGON#{\"channel\":%d,\"temp\":%f,\"humidity\":%f}", s - > getChannel(), s - > getTemperature(), s - > getHumidity());
-        ret = mosquitto_publish(mosq, NULL, & MQTT_TOPIC[0 u], strlen(valueStr), valueStr, 0, false);
-        if (ret) {
-          fprintf(stderr, "Can't publish to Mosquitto server\n");
-          exit(-1);
-        } else {
-          fprintf(stdout, "%s", valueStr);
-        }
-        delete s;
-      }
-      delay(1000);
+        delay(1000);
     }
-  }
+}
